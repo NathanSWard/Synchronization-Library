0fdd806de48ff259a004ca544d58bd65db469743
Nathan Ward <nathansward@comcast.net>
2019-08-25 15:27:48 -0600

    add lock_type vars, del mutex  operator=

--- stdlib/_mutex_base.hpp
+++ stdlib/_mutex_base.hpp
@@ -19,6 +19,8 @@ public:
         destroy_mutex(mtx_);
     }
 
+    mutex& operator=(mutex const&) = delete;
+
     void lock() {
         acquire_mutex(mtx_);
     }
@@ -43,6 +45,10 @@ private:
 struct defer_lock_t { explicit defer_lock_t() = default; };
 struct try_to_lock_t { explicit try_to_lock_t() = default; };
 struct adopt_lock_t { explicit adopt_lock_t() = default; };
+
+inline constexpr defer_lock_t defer_lock{};
+inline constexpr try_to_lock_t try_to_lock{};
+inline constexpr adopt_lock_t adopt_lock{};
 
 template<class Mutex>
 class lock_guard {
