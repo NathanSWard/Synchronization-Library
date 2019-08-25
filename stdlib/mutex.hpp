3585e76cad39399ed3bb3c9ca858af99da47b21e
Nathan Ward <nathansward@comcast.net>
2019-08-25 15:28:06 -0600

    try_lock, lock, scoped_lock implementations

--- stdlib/mutex.hpp
+++ stdlib/mutex.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "_mutex_base.hpp"
+#include <thread>
+#include <tuple>
 
 namespace sync {
 
@@ -17,6 +19,8 @@ public:
     ~recursive_mutex() {
         destroy_mutex(mtx_);
     }
+
+    recursive_mutex& operator=(recursive_mutex const&) = delete;
 
     void lock() {
         acquire_mutex(mtx_);
@@ -93,11 +97,191 @@ private:
 
 // class recursive_timed_mutex;
 
-// try_lock()
+// locking functions
 
-// lock()
+template<class M0, class M1>
+int try_lock(M0& m0, M1& m1) {
+    unique_lock lock{m0, try_to_lock};
+    if (lock.owns_lock()) {
+        if (m1.try_lock()) {
+            m0.release();
+            return -1;
+        }
+        else
+            return 1;
+    }
+    return 0;
+}
 
-// class scoped_lock;
+template<class M0, class M1, class M2, class... MN>
+int try_lock(M0& m0, M1& m1, M2& m2, MN&... mn) {
+    int i{0};
+    unique_lock lock{m0, try_to_lock};
+    if (lock.owns_lock()) {
+        i = try_lock(m1, m2, mn...);
+        if (i == -1)
+            lock.release();
+        else
+            ++i;
+    }
+    return i;
+}
+
+template<class M0, class M1>
+void lock(M0& m0, M1& m1) {
+    for (;;) {
+        {
+            unique_lock lock(m0);
+            if (m1.try_lock())
+            {
+                lock.release();
+                break;
+            }
+        }
+        std::this_thread::yield();
+        {
+            unique_lock lock(m1);
+            if (m0.try_lock())
+            {
+                lock.release();
+                break;
+            }
+        }
+       std::this_thread::yield();
+    }
+}
+
+template<class M0, class M1, class M2, class ...MN>
+void _lock_first(int i, M0& m0, M1& m1, M2& m2, MN& ...mn) {
+    for (;;) {
+        switch (i) {
+            case 0:
+                {
+                    unique_lock lock(m0);
+                    i = try_lock(m1, m2, mn...);
+                    if (i == -1) {
+                        lock.release();
+                        return;
+                    }
+                }
+                ++i;
+                std::this_thread::yield();
+                break;
+            case 1:
+                {
+                    unique_lock lock(m1);
+                    i = try_lock(m2, mn..., m0);
+                    if (i == -1) {
+                        lock.release();
+                        return;
+                    }
+                }
+                if (i == sizeof...(MN) + 1)
+                    i = 0;
+                else
+                    i += 2;
+                std::this_thread::yield();
+                break;
+            default:
+                _lock_first(i - 2, m2, mn..., m0, m1);
+                return;
+        }
+    }
+}
+
+template<class M0, class M1, class M2, class ...MN>
+inline void lock(M0& m0, M1& m1, M2& m2, MN& ...mn) {
+    _lock_first(0, m0, m1, m2, mn...);
+}
+
+template<class M0>
+inline void _unlock(M0& m0) {
+    m0.unlock();
+}
+
+template<class M0, class M1>
+inline void _unlock(M0& m0, M1& m1) {
+    m0.unlock();
+    m1.unlock();
+}
+
+template<class M0, class M1, class M2, class ...MN>
+inline void _unlock(M0& m0, M1& m1, M2& m2, MN&... mn) {
+    m0.unlock();
+    m1.unlock();
+    _unlock(m2, mn...);
+}
+
+// scoped_lock
+
+template<class ...MutexTypes>
+class scoped_lock;
+
+template<>
+class scoped_lock<> {
+public:
+    explicit scoped_lock() {}
+    ~scoped_lock() = default;
+
+    explicit scoped_lock(adopt_lock_t) {}
+
+    scoped_lock(scoped_lock const&) = delete;
+    scoped_lock& operator=(scoped_lock const&) = delete;
+};
+
+template<typename Mutex>
+class scoped_lock<Mutex> {
+public:
+    using mutex_type = Mutex;
+
+    explicit scoped_lock(mutex_type& mtx)
+        : mtx_{mtx}
+    {
+        mtx_.lock();
+    }
+
+    explicit scoped_lock(adopt_lock_t, mutex_type& mtx)
+        : mtx_{mtx}
+    {}
+
+    scoped_lock(scoped_lock const&) = delete;
+
+    ~scoped_lock() {
+        mtx_.unlock();
+    }
+
+    scoped_lock& operator=(scoped_lock const&) = delete;
+
+private:
+    mutex_type& mtx_;
+};
+
+template<class... MutexTypes>
+class scoped_lock {
+    static_assert(sizeof...(MutexTypes) > 1, "At least 2 mutex types required.");
+
+public:
+    explicit scoped_lock(MutexTypes&... mtxs)
+        : mtxs_{mtxs...}
+    {
+        lock(mtxs...);
+    }
+
+    scoped_lock(adopt_lock_t, MutexTypes&... mtxs)
+        : mtxs_{mtxs...}
+    {}
+
+    scoped_lock(scoped_lock const&) = delete;
+
+    ~scoped_lock() {
+        std::apply(_unlock<MutexTypes...>, mtxs_);
+    }
+
+    scoped_lock& operator=(scoped_lock const&) = delete;
+
+private:
+    std::tuple<MutexTypes&...> mtxs_;
+};
 
 // class call_once;
 
